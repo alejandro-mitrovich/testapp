80e27368b2cfda57f55a24e7e9148e26b171cb84
jejoon yoo <jejoony@gmail.com>
2012-07-09 20:43:31 -0400

    Practice scaffolding and unit testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '3e770a66bf78758caa9689b49c547ef615e5e0df71818bc46db7800d1a4bd06250270f46116c1884bf05d009aa31b57f6005904d480062f460d6e0c08114481f'
